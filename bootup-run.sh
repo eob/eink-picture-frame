93cdbbd7d7f36c8f5bdb0a10c1a43086931f0fd2
Ted Benson <edward.benson@gmail.com>
2025-03-02 11:54:49 -0500

    Update log

--- bootup-run.sh
+++ bootup-run.sh
@@ -2,5 +2,6 @@
 
 # This is intended to be run at bootup by the script in /etc/rc.local
 #
-sudo bash ./scripts/start.sh > /home/ted/Desktop/eink-picture-frame/piink-log.txt 2>&1 &
+touch log.txt
+sudo bash ./scripts/start.sh > ./log.txt 2>&1 &
 exit 0
